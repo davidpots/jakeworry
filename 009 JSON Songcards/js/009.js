6e18f3ceecd361e471183e46bebb83f89ec00c56
David Potsiadlo <davidpots@gmail.com>
2013-08-27 10:45:15 -0400

    Starts 009, the JSON songcard exercise.

---
+++ 009 JSON Songcards/js/009.js
@@ -0,0 +1,66 @@
+// Variable preparation station
+var $lines = $('#songcard p span'), i, len = $lines.length;
+
+// This function resizes all the lines
+function resizeLines(){
+  for(i = 0; i < len; i++) {
+    
+    // Loop item
+    var $line = $($lines[i]);
+
+    // Sets the width of each line
+    var baseWidth = 50,
+        randomRange = 40,
+        numRand = baseWidth + Math.floor(Math.random()*randomRange)+'%';
+    $line.animate({width:numRand});
+  }
+}
+
+
+$.getJSON('data.json', function(json) {
+
+  // ===============================================
+  // Count the # of items in the JSON
+  // ===============================================
+
+        var size = 0;
+        $.each(json.songs, function(i) {
+          size++;
+        });
+
+  // ===============================================
+  // Show a Song
+  // ===============================================
+
+        function updateCard(){
+          var randEntry = Math.floor(Math.random()*size+1);
+          var randomSong = json.songs[randEntry-1].title;
+          $('#title').html(randEntry).html(randomSong);
+        }
+
+  // ===============================================
+  // ON PAGE LOAD: Fade card in + show random song title
+  // ===============================================
+
+        updateCard();
+        resizeLines();
+        $('#songcard').fadeIn(400);
+
+  // ===============================================
+  // ON CLICK: Show new song
+  // ===============================================
+
+        function hideSong(){
+
+        }
+
+        $('body').click(function(){
+          $('.fdr').fadeOut(200, function() {
+            updateCard();
+          });
+          $('.fdr').fadeIn(200);
+          // resizeLines();
+        });
+
+
+});
