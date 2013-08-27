188c116c02f4e8a1b7942f87ea6ec22665e080d4
David Potsiadlo <davidpots@gmail.com>
2013-08-27 08:27:40 -0400

    Exploring a bit more animation effects...

--- 007 Animation, part 1/javascript/007.js
+++ 007 Animation, part 1/javascript/007.js
@@ -9,8 +9,8 @@ function resizeLines(){
     var $line = $($lines[i]);
 
     // Sets the width of each line
-    var baseWidth = 30,
-        randomRange = 61,
+    var baseWidth = 10,
+        randomRange = 90,
         numRand = baseWidth + Math.floor(Math.random()*randomRange)+'%';
     $line.animate({width:numRand});
   }
@@ -25,5 +25,10 @@ $(document).ready(function(){
 $('.song').click(function(){
   resizeLines();
   $('.song').toggleClass('colored');
+
+  // This changes the text of the H1 after it has faded out... then fades it back in
+  $.when($('.song h1').fadeOut(200)).done(function(){
+    $('.song h1').html("Pinball Wizard").fadeIn(200);
+  });
   return false;
 });
