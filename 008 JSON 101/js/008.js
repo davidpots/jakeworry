98c079b7d2b8c93c4bcb235b2dbac68c7cd79bb4
David Potsiadlo <davidpots@gmail.com>
2013-08-30 16:00:05 -0400

    Cleans up 008 and adds 012 (Mustache + JSON)

--- 008 JSON 101/js/008.js
+++ 008 JSON 101/js/008.js
@@ -1,30 +1,40 @@
-// set the variables
-var data, template, html;  
+    $.getJSON('data.json', function(json) {
 
-// our data! 
-data = {  
-    title : "Albums by The Doors",  
-    albums: [  "The Doors", 
-              "Strange Days", 
-              "Waiting for the Sun", 
-              "The Soft Parade", 
-              "Morrison Hotel", 
-              "LA Woman"
-            ]
-};
+      // ===============================================
+      // Count the # of items in the JSON
+      // ===============================================
 
-// core template of the HTML we'll write into  
-template = "\
-<h1>{{title}}</h1>\
-<ul>\
-  {{#albums}}\
-  <li> {{.}} </li>\
-  {{/albums}}\
-</ul>\
-";
+            var size = 0;
+            $.each(json.users, function(i) {
+              size++;
+            });
 
-// the HTML that gets spit out
-html = Mustache.to_html(template, data);  
-  
-// put that HTML into the DOM
-document.getElementById('target').innerHTML = html;
+      // ===============================================
+      // Output all items in the JSON
+      // ===============================================
+
+            var output="<ul>";
+            for (var i in json.users) {
+                output+="<li>" + json.users[i].firstName + " " + json.users[i].lastName + " – " + json.users[i].joined.month+"</li>";
+            }
+            output+="</ul>";
+
+            document.getElementById("numObjects").innerHTML=size;
+            document.getElementById("placeholder").innerHTML=output;
+
+      // ===============================================
+      // Randomly select + display one of the users
+      // ===============================================
+
+            var randEntry = Math.floor(Math.random()*size+1);
+            var randomUserName = json.users[randEntry-1].firstName + " " + json.users[randEntry-1].lastName;
+
+            $('#user_random').html(randEntry).html(randEntry + " (" + randomUserName+ ")" );
+
+      // ===============================================
+      // Fade in the content, so it is smooth like butter
+      // ===============================================
+
+            $('.dynamic').fadeIn(400);
+
+    });
---
+++ 012 JSON + Mustache/js/012.js
@@ -0,0 +1,37 @@
+var data, size = 0;
+
+  // ===============================================
+  // Fetch the data
+  // ===============================================
+
+      $.ajax({
+        async: false,
+        url: 'data.json',
+        success: function(myJson) {
+          data = myJson;
+        }
+      });
+
+
+// core template of the HTML we'll write into  
+template = "\
+<ul>\
+  {{#songs}}\
+  <li> {{song_title}} by {{song_artist_name}} </li>\
+  {{/songs}}\
+</ul>\
+";
+
+// the HTML that gets spit out
+html = Mustache.to_html(template, data);  
+  
+// put that HTML into the DOM
+document.getElementById('target').innerHTML = html;
+
+// ------------------
+
+// To do:
+// 1. count the songs
+// 2. randomly select a song
+// 3. how to sort them by name || artist || year?
+// 4. how to randomly sort them?
