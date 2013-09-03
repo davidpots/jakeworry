aac3dbb6a3584725d66f8ebde111c29bc9813e0d
David Potsiadlo <davidpots@gmail.com>
2013-09-03 10:08:12 -0400

    Updates 013 with alternative way of sorting

--- 013 JSON Sorting/js/013.js
+++ 013 JSON Sorting/js/013.js
@@ -1,5 +1,39 @@
+var data;
+
+// ===============================================
+  // Fetch the data
+  // ===============================================
+
+      $.ajax({
+        async: false,
+        url: 'people.json',
+        success: function(myJson) {
+          data = myJson;
+        }
+      });
+
+
+
+      
+
+
 // I started with this one but ran into string/object/array headaches
 // http://jsfiddle.net/VAKrE/378/
+function sortJSON(data, key) {
+    return data.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
+console.log("Before sort: " + JSON.stringify(data));
+sortJSON(data.people, 'myKey');
+console.log("After sort: " + JSON.stringify(data));
+
+
+
+
+
+
 
 // Then found this one which works but seems like it may be overkill for my current barebones requirements. Mess with stripping down later?
 // via http://stackoverflow.com/questions/979256/sorting-an-array-of-javascript-objects
@@ -11,67 +45,13 @@ var sort_by = function(field, reverse, primer){
    }
 }
 
-
-// Now you can sort by any field at will...
-var homes = [{
-
-   "h_id": "3",
-   "city": "Dallas",
-   "state": "TX",
-   "zip": "75201",
-   "price": "2500"
-
-}, {
-
-   "h_id": "4",
-   "city": "Alabama",
-   "state": "CA",
-   "zip": "90210",
-   "price": "319250"
-
-},{
-
-   "h_id": "4",
-   "city": "Maryland",
-   "state": "CA",
-   "zip": "90210",
-   "price": "87519250"
-
-},{
-
-   "h_id": "4",
-   "city": "Zaire",
-   "state": "CA",
-   "zip": "90210",
-   "price": "519250"
-
-},{
-
-   "h_id": "4",
-   "city": "Bevery Hills",
-   "state": "CA",
-   "zip": "90210",
-   "price": "19250"
-
-}, {
-
-   "h_id": "5",
-   "city": "New York",
-   "state": "NY",
-   "zip": "00010",
-   "price": "4962500"
-
-}];
-
 // Sort by price, high to low
-homes.sort(sort_by('price', false, parseInt));
-
+data.people.sort(sort_by('price', false, parseInt));
 // Sort by city, A-Z
-homes.sort(sort_by('city', true));
-
+data.people.sort(sort_by('myKey', true));
 // etc
 
-for (var i = 0; i < homes.length; i++) {
+for (var i = 0; i < data.people.length; i++) {
     // Loop through array, display city name
-    console.log(homes[i].city);
+    console.log(data.people[i].myKey);
 }
