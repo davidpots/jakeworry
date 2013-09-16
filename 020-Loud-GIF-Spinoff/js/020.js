9aa6bb983efc7248c70c6482d59271221438275b
David Potsiadlo <davidpots@gmail.com>
2013-09-16 11:46:02 -0400

    Progress with 020 -- gif parade

--- 020-Loud-GIF-Spinoff/js/020.js
+++ 020-Loud-GIF-Spinoff/js/020.js
@@ -8,42 +8,87 @@ function makeEmbeddable(code) {
   return "//www.youtube.com/embed/"+code;
 }
 
-$('.yt-link').click(function(){ 
-  var str = $(this).attr('href');
-  str = getYouTubeCode(str);
-  str = makeEmbeddable(str);
-  str = "https:" + str + "?autoplay=1&amp;controls=0&amp;modestbranding=1&amp;playlist=s7L2PVdrb_8&amp;showinfo=0&amp;start=0&amp;loop=1&amp;enablejsapi=1"
-  // alert(str);
+// Prototype: if user clicks on YouTube link, change the background video
 
-  $('#player').attr('src',str)
+    $('.yt-link').click(function(){ 
+      var str = $(this).attr('href');
+      str = getYouTubeCode(str);
+      str = makeEmbeddable(str);
+      str = "https:" + str + "?enablejsapi=1&controls=0&modestbranding=1&autoplay=1&start=0&origin="+location.hostname;
+      $('#player').attr('src',str)
+      return false;
+    });
 
-  // var iframe = document.getElementById('player');
-  // iframe.src = str;
+// Read URL params, v1
+// via http://jquery-howto.blogspot.com/2009/09/get-url-parameters-values-with-jquery.html
+    
+    function getUrlVars() {
+      // look for comment by SpaceLobster to make the orig post's code return an object instead of array
+      var params = {}, d = function (s) { return s ? decodeURIComponent(s.replace(/\+/, " ")) : null; }
+      if(window.location.search) $.each(window.location.search.substring(1).split('&'), function(i, v) {
+      var pair = v.split('=');
+      params[d(pair[0])] = d(pair[1]);
+      });
+      return params;
+    }
+    var urlVars = getUrlVars();
 
-  return false;
-});
+// Read URL params, v2
+// via LoudGIF http://loudgif.com/
+    function getParam(name) {
+        return decodeURI(
+            (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+        );
+    }
 
-//{{youTubeVideoId}}
+// Read URL params, v3
+// replaces each "+"" with human-readable spaces
+// via http://stackoverflow.com/a/4458580
+    function presto(str) {
+      return decodeURIComponent((str+'').replace(/\+/g, '%20'));
+    }
 
-// data = {  
-//     videos: [ "The Doors", 
-//               "Strange Days", 
-//               "Waiting for the Sun", 
-//               "The Soft Parade", 
-//               "Morrison Hotel", 
-//               "LA Woman"
-//             ]
-// };
 
-// SRC has to be this:
-// https://www.youtube.com/embed/{{youTubeVideoId}}?autoplay=1&amp;controls=0&amp;modestbranding=1&amp;playlist=s7L2PVdrb_8&amp;showinfo=0&amp;start=0&amp;loop=1&amp;enablejsapi=1
+// Turn Params into vars
 
-// var bananaFace = function(data){
+    // var gif_url;
 
-//   // grab the template
-//   var template = $('#youTubePlayer').html();
+    // $("#newParade-form").submit(function(e){
+    //   gif_url = getParam(gif_url);
+    //   if (gif_url != '') {
+    //     alert("This URL of the gif is " + getParam(gif_url));
+    //   }
+    // });
 
-//   // write to the page
-//   $('#playerWrap').append( Mustache.to_html(template,presto) );
+    // if (getParam('gif_url') == "null") {
+    //   alert("it is null!");
+    // }
 
-// };
+    if (urlVars.paradeTitle != undefined) {
+      var str = presto(urlVars.paradeTitle);
+      $('#chyron h1').text(str);
+      $('input[name="paradeTitle"]').attr('value',str);
+    }
+
+    if (urlVars.paradeGifUrl != undefined) {
+      var strUrl = presto(urlVars.paradeGifUrl);
+      var strCss = "background-image: url("+strUrl+")";
+      $('#gif').attr('style',strCss);
+      $('input[name="paradeGifUrl"]').attr('value',strUrl);
+    }
+
+    // var paradeTitle = getParam('paradeTitle');
+    // if (paradeTitle == "null") {
+    //   //
+    // } else {
+    //   $('#chyron h1').text(paradeTitle);
+    // }
+
+
+    // var paradeGifUrl = getParam('paradeGifUrl');
+    // if (paradeGifUrl == "null") {
+    //   //
+    // } else {
+    //   str = "background-image: url("+paradeGifUrl+")";
+    //   $('#gif').attr('style',str);
+    // }
