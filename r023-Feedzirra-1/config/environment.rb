4b6231046cd9fecf62ec6867d367c7d961a1406c
David Potsiadlo <davidpots@gmail.com>
2013-09-25 08:49:31 -0400

    Intro to using Rails / Feedzirra to fetch remote XML

---
+++ r023-Feedzirra-1/config/environment.rb
@@ -0,0 +1,8 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+R023Feedzirra1::Application.initialize!
+
+# Setup the Feedzirra gem, as per http://railscasts.com/episodes/168-feed-parsing?view=asciicast
+# config.gem "feedzirra", :lib => "feedzirra", :source => "http://gems.github.com"
---
+++ r023-Feedzirra-1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R023Feedzirra1::Application.config.secret_token = 'f4bb1c04a155033d18e771aa7a5420e12865776275ba0d90534a292440b273cbe0a659ee5aac771205ae0c115ab7d43a151f4c42bef075ead62f82bf58125965'
