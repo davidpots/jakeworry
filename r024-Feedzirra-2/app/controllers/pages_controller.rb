1d815daf86158b92414d92864a5b8af08b61ef06
David Potsiadlo <davidpots@gmail.com>
2013-09-26 16:33:46 -0400

    Gets feed parsing working! Phew

--- r024-Feedzirra-2/app/controllers/pages_controller.rb
+++ r024-Feedzirra-2/app/controllers/pages_controller.rb
@@ -5,26 +5,49 @@ require 'open-uri'
 
 class PagesController < ApplicationController
   def home
+    # feed_url = 'http://joeroganexp.joerogan.libsynpro.com/rss'
 
-    doc = Nokogiri::HTML(open("http://www.threescompany.com/"))
+    output = "<h1>My Podcasts</h1>" 
 
-    # feed_url = 'http://mysteriousuniverse.org/feed/podcast'
-    feed_url = 'http://joeroganexp.joerogan.libsynpro.com/rss'
-    output = "<h1>My RSS Reader</h1>" 
-    open(feed_url) do |http|
-      response = http.read
-      result = RSS::Parser.parse(response, false)
-      # @feed = result
-      output += "<strong>Title:</strong> #{result.channel.title}<br />"
-      output += "<strong>Link:</strong> #{result.channel.link}<br />"
-      output += "<strong>Description:</strong> #{result.channel.image.url}<br />"
-      output += "<br />"
-      # @dog = result.channel./itunes:summary
-      # result.items.each_with_index do |item, i|
-      #   output += "#{i+1}. #{item.title}<br />" if i < 10  
-      # end
+    feeds = [ "http://feeds.feedburner.com/TheDrunkenTaoistPodcast",
+              "http://lavenderhour.libsyn.com/rss",
+              "http://joeroganexp.joerogan.libsynpro.com/rss",
+              "http://www.londonreal.tv/feed/podcast/",
+              "http://midwestreal.libsyn.com/rss",
+              "http://mysteriousuniverse.org/feed/podcast",
+              "http://www.castmate.fm/feed.php?u=drchrisryanpodcast",
+              "http://warriorpoet.podomatic.com/rss2.xml" ]
+
+    feeds.each do |feed|  
+      open(feed) do |http|
+        response = http.read
+        result = RSS::Parser.parse(response, false)
+
+        @result_channel_title = result.channel.title
+        @result_channel_link  = result.channel.link
+        if result.channel.image #&& result.channel.image.url
+          @result_channel_image_url = result.channel.image.url
+        end
+        @result_channel_lastBuildDate = result.channel.lastBuildDate
+        @result_channel_description = result.channel.description
+
+        output += "<strong>Title:</strong> #{@result_channel_title}<br />"
+        output += "<strong>Link:</strong> #{@result_channel_link}<br />"
+        if @result_channel_image_url.nil?
+          output += "it is nil!<br />"
+        else 
+          output += "#{@result_channel_image_url}<br />"
+        end
+        # result.channel.image.url.empty? ? nil : output += "<strong>Image:</strong> #{result.channel.image.url}<br />"
+        output += "<strong>Last Build Date:</strong> #{@result_channel_lastBuildDate}<br />"
+        output += "<strong>Description:</strong> #{@result_channel_description}<br />"
+        output += "<br />"
+        # result.items.each_with_index do |item, i|
+        #   output += "#{i+1}. #{item.title}<br />" if i < 10  
+        # end
+      end
+      @feed = output.html_safe
     end
-    @feed = output.html_safe
 
   end
 end
