91435f9c4f863376ce0e51895b1b7857a73d524a
David Potsiadlo <davidpots@gmail.com>
2013-09-26 10:46:51 -0400

    More progress learning about parsing, and the itunes namespace headache kinda solved

--- r024-Feedzirra-2/app/controllers/pages_controller.rb
+++ r024-Feedzirra-2/app/controllers/pages_controller.rb
@@ -6,16 +6,20 @@ require 'open-uri'
 class PagesController < ApplicationController
   def home
 
-    feed_url = 'http://mysteriousuniverse.org/feed/podcast'
+    doc = Nokogiri::HTML(open("http://www.threescompany.com/"))
+
+    # feed_url = 'http://mysteriousuniverse.org/feed/podcast'
+    feed_url = 'http://joeroganexp.joerogan.libsynpro.com/rss'
     output = "<h1>My RSS Reader</h1>" 
     open(feed_url) do |http|
       response = http.read
       result = RSS::Parser.parse(response, false)
+      # @feed = result
       output += "<strong>Title:</strong> #{result.channel.title}<br />"
       output += "<strong>Link:</strong> #{result.channel.link}<br />"
-      output += "<strong>Description:</strong> #{result.channel.description}<br />"
+      output += "<strong>Description:</strong> #{result.channel.image.url}<br />"
       output += "<br />"
-      @dog = result.channel./itunes:summary
+      # @dog = result.channel./itunes:summary
       # result.items.each_with_index do |item, i|
       #   output += "#{i+1}. #{item.title}<br />" if i < 10  
       # end
