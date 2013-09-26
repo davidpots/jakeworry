e3fbe0d7b593ade04ea14157436cf8912725cf05
David Potsiadlo <davidpots@gmail.com>
2013-09-26 10:15:00 -0400

    Steps forward and backward getting parsing to work -- hung up on namespaces

--- r024-Feedzirra-2/app/controllers/pages_controller.rb
+++ r024-Feedzirra-2/app/controllers/pages_controller.rb
@@ -1,4 +1,26 @@
+# via http://www.robbyonrails.com/articles/2005/05/11/parsing-a-rss-feed
+
+require 'rss/2.0'
+require 'open-uri'
+
 class PagesController < ApplicationController
   def home
+
+    feed_url = 'http://mysteriousuniverse.org/feed/podcast'
+    output = "<h1>My RSS Reader</h1>" 
+    open(feed_url) do |http|
+      response = http.read
+      result = RSS::Parser.parse(response, false)
+      output += "<strong>Title:</strong> #{result.channel.title}<br />"
+      output += "<strong>Link:</strong> #{result.channel.link}<br />"
+      output += "<strong>Description:</strong> #{result.channel.description}<br />"
+      output += "<br />"
+      @dog = result.channel./itunes:summary
+      # result.items.each_with_index do |item, i|
+      #   output += "#{i+1}. #{item.title}<br />" if i < 10  
+      # end
+    end
+    @feed = output.html_safe
+
   end
 end
--- r024-Feedzirra-2/app/models/podcast.rb
+++ r024-Feedzirra-2/app/models/podcast.rb
@@ -1,2 +1,12 @@
 class Podcast < ActiveRecord::Base
+
+  def self.update_from_feed(feed_url)
+    feed = Feedzirra::Feed.fetch_and_parse(feed_url)
+    create!(
+      :name         => feed.title,
+      :url_web      => feed.url,
+      :description  => feed.description
+    )
+  end
+
 end
