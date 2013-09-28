01df97624a2e08d166a07238a7a0c04f550cc152
David Potsiadlo <davidpots@gmail.com>
2013-09-27 21:47:02 -0400

    Sets up app + gets homepage ready

---
+++ r025-RSS-parsing-3/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+R025RssParsing3::Application.config.secret_key_base = '38ca056f32705571a9fe6a2f4ed2139e4a773bdc417713790bd3b199cc97705fed5f3431f819c9e604b2f0667e3503fe9f3a4d19f091e39d4d4fd3f4ef92a4b3'
---
+++ r025-RSS-parsing-3/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+R025RssParsing3::Application.config.session_store :cookie_store, key: '_r025-RSS-parsing-3_session'
