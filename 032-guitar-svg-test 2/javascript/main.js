f00285a31a8aa07b0b96c730d1c97e7e1e9d441f
David Potsiadlo <davidpots@gmail.com>
2014-01-27 08:50:27 -0500

    Starts 032, part 2 of guitar fretboard ASCII SVG project

---
+++ 032-guitar-svg-test 2/javascript/main.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function(){
+
+  // Read ASCII fretboard text & prep it
+
+          // Read the ASCII fretboard that'll be replaced
+          var toReplace = $('.replaceMe').html();
+          // Split the ASCII freboard text into an array of individual characters
+          toReplace = toReplace.split('');
+
+  // Go through ASCII characters, inject HTML+SVG as needed
+
+          var replacement = "";
+          $.each( toReplace, function(i,el) {
+            if (el == "o") {
+              replacement += "<div class='dot dot-full'>"+dot+"</div>";
+            } else if (el == "^") {
+              replacement += "<div class='dot dot-full'>"+string_o+"</div>";
+            } else if (el == "x") {
+              replacement += "<div class='dot dot-full'>"+string_x+"</div>";
+            } else if ((el == "|") || (el == " ")) {
+              replacement += "<div class='dot dot-empty'></div>";
+            } 
+          });
+
+  // Update the page with the new HTML/SVG markup
+
+          $('.fretboard').append(fretboardBG);
+          $('.fretboard').append(replacement);
+
+});
