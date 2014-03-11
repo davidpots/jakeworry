110bc2c7c1449d3b1a4137ceb3ede94bce520843
David Potsiadlo <davidpots@gmail.com>
2014-03-11 10:14:36 -0400

    Adds another SVG test, renames Rails projects

---
+++ 034-guitar-svg-test-4/javascript/fretboardSVG.js
@@ -0,0 +1,97 @@
+var dot = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 52 80" enable-background="new 0 0 52 80" xml:space="preserve">\
+              <circle class="fretb_dot" fill="#27AAE1" cx="26" cy="40" r="24"/>\
+            </svg>';
+
+var dotWideLeft = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%"\
+                      height="100%" viewBox="0 0 52 80" enable-background="new 0 0 52 80" xml:space="preserve">\
+                    <g>\
+                       <path fill="#27AAE1" stroke="#27AAE1" stroke-width="2" d="M26,16C12.746,16,2,26.746,2,40s10.746,24,24,24h26V16H26z"/>\
+                    </g>\
+                  </svg>';
+
+var dotWideRight = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%"\
+                        height="100%" viewBox="0 0 52 80" enable-background="new 0 0 52 80" xml:space="preserve">\
+                      <g>\
+                         <path fill="#27AAE1" stroke="#27AAE1" stroke-width="2" d="M26,64c13.254,0,24-10.746,24-24S39.254,16,26,16H0v48H26z"/>\
+                      </g>\
+                    </svg>';
+
+var dotWideMiddle = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%"\
+                      height="100%" viewBox="0 0 52 80" enable-background="new 0 0 52 80" xml:space="preserve">\
+                      <g>\
+                        <rect x="0.001" y="16" stroke="#27AAE1" stroke-width="2" fill="#27AAE1" width="52" height="48"/>\
+                      </g>\
+                    </svg>';
+
+var string_o = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
+                      width="100%" height="100%" viewBox="0 0 52 80" enable-background="new 0 0 52 80" xml:space="preserve">\
+                  <g>\
+                     <path fill="#27AAE1" d="M26,24c5.514,0,10,4.486,10,10s-4.486,10-10,10s-10-4.486-10-10S20.486,24,26,24 M26,16\
+                     	c-9.94,0-18,8.06-18,18s8.06,18,18,18s18-8.06,18-18S35.94,16,26,16L26,16z"/>\
+                  </g>\
+                </svg>';
+
+var string_x = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
+                      width="100%" height="100%" viewBox="0 0 52 80" enable-background="new 0 0 52 80" xml:space="preserve">\
+                  <path fill="#27AAE1" d="M41.067,43.831L31.236,34l9.831-9.831c-1.365-2.087-3.149-3.871-5.236-5.236L26,28.764l-9.831-9.831\
+                     c-2.087,1.365-3.871,3.149-5.236,5.236L20.764,34l-9.831,9.831c1.365,2.087,3.149,3.871,5.236,5.236L26,39.236l9.831,9.831\
+                     C37.918,47.702,39.702,45.918,41.067,43.831z"/>\
+                </svg>';
+
+var fretb_vert_4 = '<svg class="fretb" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
+                        width="416px" height="480px" viewBox="0 0 416 480" enable-background="new 0 0 416 480" xml:space="preserve">\
+                      <g class="fretb_neckBG">\
+                         <rect x="74" y="64" fill="#fff" width="268" height="352"/>\
+                      </g>\
+                      <g class="fretb_frets">\
+                         <rect x="74" y="396" fill="#e6e6e6" width="268" height="8"/>\
+                         <rect x="74" y="316" fill="#e6e6e6" width="268" height="8"/>\
+                         <rect x="74" y="236" fill="#e6e6e6" width="268" height="8"/>\
+                         <rect x="74" y="156" fill="#e6e6e6" width="268" height="8"/>\
+                         <rect x="74" y="76"  fill="#e6e6e6" width="268" height="8"/>\
+                      </g>\
+                      <g class="fretb_top">\
+                         <rect x="74" y="64" fill="#6e6e6e" width="268" height="20"/>\
+                      </g>\
+                      <g class="fretb_strings">\
+                        <rect x="126" y="64" fill="#6e6e6e" width="8" height="352"/>\
+                        <rect x="74"  y="64" fill="#6e6e6e" width="8" height="352"/>\
+                        <rect x="230" y="64" fill="#6e6e6e" width="8" height="352"/>\
+                        <rect x="178" y="64" fill="#6e6e6e" width="8" height="352"/>\
+                        <rect x="334" y="64" fill="#6e6e6e" width="8" height="352"/>\
+                        <rect x="282" y="64" fill="#6e6e6e" width="8" height="352"/>\
+                      </g>\
+                  </svg>';
+
+var fretb_vert_12 = '<svg class="fretb" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="416px"\
+                       height="1120px" viewBox="0 0 416 1120" enable-background="new 0 0 416 1120" xml:space="preserve">\
+                      <g class="fretb_neckBG">\
+                        <rect x="74" y="64" fill="#E6E7E8" width="268" height="992"/>\
+                      </g>\
+                      <g class="fretb_frets">\
+                        <rect x="74" y="76" fill="#BCBEC0" width="268" height="8"/>\
+                        <rect x="74" y="156" fill="#BCBEC0" width="268" height="8"/>\
+                        <rect x="74" y="236" fill="#BCBEC0" width="268" height="8"/>\
+                        <rect x="74" y="316" fill="#BCBEC0" width="268" height="8"/>\
+                        <rect x="74" y="396" fill="#BCBEC0" width="268" height="8"/>\
+                        <rect x="74" y="476" fill="#BCBEC0" width="268" height="8"/>\
+                        <rect x="74" y="556" fill="#BCBEC0" width="268" height="8"/>\
+                        <rect x="74" y="636" fill="#BCBEC0" width="268" height="8"/>\
+                        <rect x="74" y="716" fill="#BCBEC0" width="268" height="8"/>\
+                        <rect x="74" y="796" fill="#BCBEC0" width="268" height="8"/>\
+                        <rect x="74" y="876" fill="#BCBEC0" width="268" height="8"/>\
+                        <rect x="74" y="956" fill="#BCBEC0" width="268" height="8"/>\
+                        <rect x="74" y="1036" fill="#BCBEC0" width="268" height="8"/>\
+                      </g>\
+                      <g class="fretb_top">\
+                        <rect id="Top_Lip_3_" x="74" y="64" fill="#58595B" width="268" height="20"/>\
+                      </g>\
+                      <g class="fretb_strings">\
+                        <rect x="126" y="64" fill="#58595B" width="8" height="992"/>\
+                        <rect x="74" y="64" fill="#58595B" width="8" height="992"/>\
+                        <rect x="230" y="64" fill="#58595B" width="8" height="992"/>\
+                        <rect x="178" y="64" fill="#58595B" width="8" height="992"/>\
+                        <rect x="334" y="64" fill="#58595B" width="8" height="992"/>\
+                        <rect x="282" y="64" fill="#58595B" width="8" height="992"/>\
+                      </g>\
+                    </svg>';
